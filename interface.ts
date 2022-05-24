a0cb8f4d5d6fd8230f5390993f8de580ebbda5da
felixd3v <fmhcde@gmail.com>
2022-05-24 15:11:55 -0500

    first commit

---
+++ interface.ts
@@ -0,0 +1,11 @@
+interface Contact {
+    id: number;
+    name: string;
+    birthDate: Date;
+}
+
+let firstContact: Contact = {
+    birthDate: new Date("12-12-1912"),
+    id: 324,
+    name: "John Doe"
+}
